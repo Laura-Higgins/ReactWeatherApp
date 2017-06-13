b9871f5dd95be2bef1fc957f747867998fb62b0c
Laura Higgins <lauraghiggins@me.com>
2017-06-13 16:40:41 -0700

    added Sparkline npm.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import {Sparklines, SparklinesLine} from 'react-sparklines'
 
 import { connect } from  'react-redux'
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name
+    const temps = cityData.list.map(weather => weather.main.temp)
 
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={140} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
       </tr>
     )
   }
